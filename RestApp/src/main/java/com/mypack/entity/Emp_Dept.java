4c45ec49eaa229af4a789ef9707ba94b11924ee9
deepakmca <deepakguptag96@gmail.com>
2018-04-27 22:45:11 +0530

    second commit

---
+++ RestApp/src/main/java/com/mypack/entity/Emp_Dept.java
@@ -0,0 +1,10 @@
+package com.mypack.entity;
+
+public class Emp_Dept {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Message to check git is working or not.....");
+
+	}
+
+}
